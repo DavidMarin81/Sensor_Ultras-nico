467aa3a56432479d3f57b44e563296ee53199b2a
David Marín Gómez <91268739+DavidMarin81@users.noreply.github.com>
2023-07-17 10:57:49 +0200

    Update proyectoBluetoothEsclavo.ino

--- proyectoBluetoothEsclavo.ino
+++ proyectoBluetoothEsclavo.ino
@@ -19,20 +19,20 @@ void setup() {
 
 void loop() {
   digitalWrite(TRIG, HIGH);
-  delay(1);
+  delay(250);
   digitalWrite(TRIG, LOW);
   DURACION = pulseIn(ECO, HIGH); //Devuelve un valor en microsegundos
   DISTANCIA = DURACION / 58.2;   // 58.2 es el valor por el que se divide para que de la distancia en cms. 
                                  // Lo da las especificaciones
   Serial.println(DISTANCIA);     // Imprime en el Serial la distancia que va recogiendo
-  delay(500);                    // Tiempo de espera para imprimir en el serial
+  delay(50);                    // Tiempo de espera para imprimir en el serial
 
-  if(DISTANCIA <= 105 && DISTANCIA > 3){  // Si el sensor está fuera de rango, devuelve valores negativos
+  if(DISTANCIA <= 105 && DISTANCIA > 20){  // Si el sensor está fuera de rango, devuelve valores negativos
     Serial.println("Detectando movimiento");
       BT1.write('1');
       delay(5000);
     } else {
       BT1.write('2');
       delay(250);
-    }  
+    }
 }
